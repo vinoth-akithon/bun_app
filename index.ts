999d7f3815eaf9f427eb4c42c7474a6c42b5a329
vinoth kumar <vinoth.akithon@gmail.com>
2025-09-28 02:15:41 +0530

    Adding `concurrently` for starting applications in a single turn

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import concurrently from "concurrently";
+
+concurrently([
+    {
+        name: "Server",
+        command: "bun start",
+        cwd: "packages/server",
+        prefixColor: "green"
+    },
+    {
+        name: "Client",
+        command: "bun start",
+        cwd: "packages/client",
+        prefixColor: "yellow"
+    }
+])
